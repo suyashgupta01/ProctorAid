f10478684fd317d25ea36e41033af58aaef620ff
Suyash Gupta <suyashgupta0001@gmail.com>
2022-05-06 20:15:11 +0530

    browser thing working

--- browser.py
+++ browser.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
+import time
 from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
 
 def open_browser(url):
     driver = webdriver.Chrome(ChromeDriverManager().install()) 
-    driver.get(url)
+    # chrome_options = Options()
+    # chrome_options.add_experimental_option("detach", True) # to keep window open :)
+    try:
+        driver.get(url)
+    except:
+        driver.get(url) # just reload if ek bar me load na ho
+    time.sleep(5) # wait for browser to load :)
+    while(True): # to keep the browser window open, else it'll get garbage collected as it's not reference anywhere else in main.py | https://stackoverflow.com/a/47509389/11684146
+       pass
--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 # https://docs.python.org/3/library/multiprocessing.html
 
 import multiprocessing as mp, sys, time # Python's libraries
-import keylogger, face_detection, window_monitor, pc_locker # My libraries
+import keylogger, face_detection, window_monitor, pc_locker, browser # My libraries
 
 def all_processes_alive(list_of_processes):
     for p in list_of_processes:
@@ -17,7 +17,7 @@ def all_processes_alive(list_of_processes):
     print("returning True")    
     return True
 
-def kill_all_processes(list_of_processes):
+def kill_processes(list_of_processes):
     print("killing all processes!")
     for p in list_of_processes:
         p.terminate() # terminate is like "shut down", kill is like "turn off switch" | https://stackoverflow.com/a/19206399/11684146
@@ -27,37 +27,46 @@ def kill_all_processes(list_of_processes):
 # https://docs.python.org/3/library/multiprocessing.html#multiprocessing-programming
 if __name__ == "__main__":
 
+    exactly_allowed_titles=[]; partially_allowed_titles=['Code', 'Brave', 'Chrome']
+    exam_url = "https://suyashgupta.me"
+    close_browser = True # close browser window if user's endulging in malpractices
+    lock_pc = False # lock PC if user's endulging in malpractices
+
     # create a process
+    p0 = mp.Process(target=browser.open_browser, args = (exam_url,))
     p1 = mp.Process(target=keylogger.log_keys)
-    exactly_allowed_titles=[]; partially_allowed_titles=['Code', 'Brave']
-    p2 = mp.Process(target=window_monitor.check_illegal_window, args=(exactly_allowed_titles, partially_allowed_titles))
+    p2 = mp.Process(target=window_monitor.check_illegal_window, args = (exactly_allowed_titles, partially_allowed_titles))
     p3 = mp.Process(target=face_detection.face_monitoring)
+
 
     # set process as daemon to allow the process to run in the background
     # by settting it as daemon, Python will not run p.join() implicitly
+    p0.daemon = True
     p1.daemon = True
     p2.daemon = True
     p3.daemon = True
 
-    # start that process
+    p0.start()
     p1.start()
     p2.start()
     p3.start()
 
     # The join() call ensures that subsequent lines of your code are not called before all the multiprocessing processes are completed. 
     # https://stackoverflow.com/a/25391661/11684146 & https://stackoverflow.com/a/60967514/11684146
-    # p1.join()
-    # p2.join()
-    # p3.join()
+    # p.join()
 
     # Ensure that all processes are running
-    # Kill all processes even if one process dies
+    # Kill all processes (EXCEPT BROWSER VALA p0) even if one process dies
     while True:
         time.sleep(0.5)
         if not all_processes_alive([p1, p2, p3]):
             time.sleep(0.05) # to prevent the main control from running ahead while processes terminate
-            print("Student is using unfair practices, locking computer now")
-            kill_all_processes([p1, p2, p3])
+            print("Student is using unfair practices!!!")
+            if close_browser:
+                kill_processes([p0, p1, p2, p3])
+            else:
+                kill_processes([p0, p1, p2, p3])
             time.sleep(0.5) # to prevent the main control from running ahead while processes terminate
-            pc_locker.lock_pc()
+            if lock_pc:
+                pc_locker.lock_pc()
             sys.exit()
